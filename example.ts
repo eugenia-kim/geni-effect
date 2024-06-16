8a418fa9049d14d1971fcfda6563c4c5dafce830
Eugenia Kim <geni950814@gmail.com>
2024-06-16 23:17:04 +0100

    [done] 3. Simple cache of function on disk

--- example.ts
+++
@@ -1,7 +0,0 @@
-import { geni } from "./index";
-import { Array, Number } from "@effect/schema/Schema";
-
-
-const flatten = await geni("Flatten an array", Array(Array(Number)), Array(Number));
-
-console.log(flatten([[1, 2, 3], [4, 5, 6]]));
--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@ import { Effect, Console } from "effect";
 import * as S from "@effect/schema/Schema";
 import { JSONSchema } from "@effect/schema";
 import OpenAI from "openai";
+import { sha1 } from "js-sha1";
 
 // Plan
 // [done] 1. First function with proper typing
 // [done] 2. Call open AI to generate a function
-// 3. Simple cache of function on disk
+// [done] 3. Simple cache of function on disk
 // 4. Generate Effect instead of the function with Success
 // 5. Generate Effect with Success, Error, and Requirements
+// 6. Replace bun file with storage abstraction using Effect platform layer
 
 const openai = new OpenAI();
 
@@ -19,6 +21,11 @@ async function request(prompt: string) {
   });
 
   return completion.choices[0].message.content || "";
+  // return `
+  // function main(arrays) {
+  //   return [...arrays.fst, ...arrays.snd];
+  // } 
+  // `;
 }
 
 const TwoArrays = S.Struct({
@@ -26,16 +33,11 @@ const TwoArrays = S.Struct({
   snd: S.Array(S.Number),
 });
 
-type TwoArraysType = S.Schema.Type<typeof TwoArrays>;
+type TwoArraysType = S.Schema.Type<typeof TwoArrays>; ``
 
-async function geni<Input, Output>(
-  description: string,
-  input: S.Schema<Input>,
-  output: S.Schema<Output>
-): Promise<(input: Input) => Output> {
-  const result = await request(`
+async function generateFunction<Input, Output>(description: string, input: S.Schema<Input>, output: S.Schema<Output>) {
+  return await request(`
   Generate a javascript function based on the given description, input schema and output schema to be passed into eval function.
-
 Example:
 
 description: 'Return a happy birthday message to the person mentioning the age.' 
@@ -54,6 +56,24 @@ description: ${description}
 input: ${input}
 output: ${output}
 `);
+}
+
+async function geni<Input, Output>(
+  description: string,
+  input: S.Schema<Input>,
+  output: S.Schema<Output>
+): Promise<(input: Input) => Output> {
+  const hash = sha1(`${description}:${input}:${output}`);
+  const file = Bun.file(`.geni/${hash}.js`);
+  const result = await (async () => {
+    if (await file.exists()) {
+      return file.text();
+    }
+    const r = await generateFunction(description, input, output);
+    Bun.write(file, r);
+    return r;
+  })();
+
   return (args: Input) => {
     const toEval = `${result} \n main(${JSON.stringify(args)}); `
     console.log(toEval);
@@ -65,3 +85,6 @@ const flatten = await geni("Flatten an array", TwoArrays, S.Array(S.Number));
 
 console.log(flatten({ fst: [1, 2, 3], snd: [4, 5, 6] }));
 
+const dotproduct = await geni("Dot product two arrays", TwoArrays, S.Array(S.Number));
+
+console.log(dotproduct({ fst: [1, 2, 3], snd: [4, 5, 6] }));
