a51b1026765453481094086b05885a88370bb18f
Eugenia Kim <geni950814@gmail.com>
2024-06-16 20:37:39 +0100

    geni calls openai to generate TS code

--- index.ts
+++ index.ts
@@ -8,6 +8,17 @@ import OpenAI from "openai";
 // 2. Call open AI to generate a function
 // 3. Simple cache of function on disk
 
+const openai = new OpenAI();
+
+async function request(prompt: string) {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: prompt }],
+    model: "gpt-4o",
+  });
+
+  return completion.choices[0].message.content;
+}
+
 const Person = S.Struct({
   name: S.String,
   age: S.Number,
@@ -15,34 +26,28 @@ const Person = S.Struct({
 
 type PersonType = S.Schema.Type<typeof Person>;
 
-const person = { name: "hello", age: 13 };
-
-const program = Console.log("Hello, World!");
-Effect.runSync(program);
-
-function geni<Input, Output>(
+async function geni<Input, Output>(
   description: string,
   input: S.Schema<Input>,
   output: S.Schema<Output>
-): (input: Input) => Output {
-  return (input: Input) => "eugenia" as Output;
+): Promise<(input: Input) => Output> {
+  const result = await request(`Don't return other than the TypeScript function code.
+   Make sure the function is called 'main'. 
+   Generate a single function which does the following: ${description}`);
+  console.log(result);
+  return (input: Input) => 'eugenia' as Output;
 }
 
-const hello = geni("hello", Person, S.String);
+const hello = await geni("Return a happy birthday message to the person mentioning the age.", Person, S.String);
 
-const result = hello(person);
+const result = hello({ name: "hello", age: 13 });
 
 console.log(result);
 
-const openai = new OpenAI();
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
 
-  console.log(completion.choices[0]);
-}
+// const r = request(
+//   `generate a function with the following schema: { description: "returns hello, name of the person", input: ${Person}, output: ${S.String} }`,
+// );
 
-main();
+// console.log(r);
