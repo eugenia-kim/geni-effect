02967e678142095bb243cae7c4915a0d7bb1ae87
Eugenia Kim <geni950814@gmail.com>
2024-06-16 22:06:47 +0100

    geni works with a single input

--- index.ts
+++ index.ts
@@ -32,27 +32,43 @@ async function geni<Input, Output>(
   output: S.Schema<Output>
 ): Promise<(input: Input) => Output> {
   const result = await request(`
-  Generate a javascript function based on the given description to be passed into eval function.
-  
-   Make sure the function is called 'main'. 
-   Generate a single function which does the following: ${description}. 
-   Make sure the response is in a text format. Not a code block.
-`);
-  const toEval = `${result} \n main('hello', 13);`
-  console.log(toEval);
-  return () => eval(toEval);
+  Generate a javascript function based on the given description, input schema and output schema to be passed into eval function.
+
+Example:
+
+description: 'Return a happy birthday message to the person mentioning the age.' 
+input: { readonly name: string; readonly age: number }
+output: string
+
+function main(person) {
+  return \`Happy birthday \${person.name}. You're now \${person.age} years old\`
 }
 
-const hello = await geni("Return a happy birthday message to the person mentioning the age.", Person, S.String);
+  
+Make sure the function is called 'main'. 
+Make sure the response is in a text format.Not a code block.
+Generate a single function with the following description, input and output schema:
+description: ${description}
+input: ${input}
+output: ${output}
+`);
+  return (args: Input) => {
+    const toEval = `${result} \n main(${JSON.stringify(args)}); `
+    console.log(toEval);
+    return eval(toEval);
+  };
+}
+
+const hello = await geni("Flatten an array", S.Array(S.Array(S.Number)), S.Array(S.Number));
 
 // const result = hello({ name: "hello", age: 13 });
 
-console.log(hello({ name: "hello", age: 13 }));
+console.log(hello([[1, 2, 3], [4, 5, 6]]));
 
 
 
 // const r = request(
-//   `generate a function with the following schema: { description: "returns hello, name of the person", input: ${Person}, output: ${S.String} }`,
+//   `generate a function with the following schema: { description: "returns hello, name of the person", input: ${ Person }, output: ${ S.String } } `,
 // );
 
 // console.log(r);
