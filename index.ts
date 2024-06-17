b1018f8b9d82eeb5c0865ea39dee79177e2c300f
Eugenia Kim <geni950814@gmail.com>
2024-06-17 13:20:24 +0100

    geni supports different input types

--- index.ts
+++ index.ts
@@ -30,6 +30,7 @@ type TwoArraysType = S.Schema.Type<typeof TwoArrays>; ``
 type InputSchema<Input> = { [I in keyof Input]: S.Schema<Input[I]> };
 
 async function generateFunction<Input, Output>(description: string, input: InputSchema<Input>, output: S.Schema<Output>) {
+  console.log("openAI request");
   return await request(`
   Generate a javascript function based on the given description, input schema and output schema to be passed into eval function.
 Example:
@@ -77,13 +78,12 @@ async function geni<Input extends unknown[], Output>(
 }
 
 const flatten = await geni("Flatten an array", [TwoArrays], S.Array(S.Number));
-
 console.log(flatten({ fst: [1, 2, 3], snd: [4, 5, 6] }));
 
 const dotproduct = await geni("Dot product two arrays", [TwoArrays], S.Array(S.Number));
 
-console.log(dotproduct({ fst: [1, 2, 3], snd: [4, 5, 6] }));
-
 const dotproductpair = await geni("Dot product two array pairs", [TwoArrays, TwoArrays], TwoArrays);
-
 console.log(dotproductpair({ fst: [1, 2, 3], snd: [4, 5, 6] }, { fst: [7, 2, 3], snd: [10, 5, 6] }));
+
+const concat = await geni("Append number to the string", [S.String, S.Number], S.String);
+console.log(concat("Hello", 42));
